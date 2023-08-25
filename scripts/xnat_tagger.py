9eee1b0a715196409fd10d1f0827d1faa5dec65a
Daniel Asay <danielasay@fas.harvard.edu>
2023-08-25 13:16:02 -0400

    removing default for config arg

--- scripts/xnat_tagger.py
+++ scripts/xnat_tagger.py
@@ -38,7 +38,7 @@ def main():
         help='Do not execute updates')  # Provide help text for dry-run argument
     parser.add_argument('--confirm', action='store_true',
         help='Prompt user to confirm every update')  # Provide help text for confirm argument
-    parser.add_argument('--config', required=True, default=config.default(),
+    parser.add_argument('--config', required=True,
         help='Filters configuration file') 
     parser.add_argument('--target-modality', choices=['t1', 't2', 'dwi', 'bold', 'all'], required=True, type=str.lower) # Require --target argument
     parser.add_argument('--label', required=True,
